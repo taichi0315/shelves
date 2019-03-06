2623eae7100ef92b4cff5e7e72b7206981bb9f46
taichi0315 <k.taichi0315@gmail.com>
2019-03-07 03:04:14 +0900

    post_detail page design change

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shelves/', include('shelves.urls')),
+    path('', include('shelves.urls')),
 ]
 
 if settings.DEBUG:
